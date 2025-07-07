8845c29f6f71f9647550e46fe96b7d775a3cfcbd
Rinlanee <Rinlanee77093@gmail.com>
2025-07-07 14:45:34 +0700

    สร้าง Container Widget

--- main.dart
+++ main.dart
@@ -9,19 +9,53 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: const Center(
-          child: Text(
-            'Hi Flutter',
-            style: TextStyle(
-              fontSize: 24,
-              color: Colors.blue,
-              fontWeight: FontWeight.bold,
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 162, 213, 255),
+        textTheme: const TextTheme(
+              bodyMedium: TextStyle(fontSize: 24, color: Colors.indigo),
+        ),
+      ),
+      home: Builder(
+      builder: (context) {
+        int index = 0;
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return Scaffold(
+              appBar: AppBar(title: const Text('Column & Row Example')),
+              body: Center(
+          child: Container(
+            width: 200,
+            height: 100,
+            margin: EdgeInsets.all(20),  
+            padding: EdgeInsets.all(10),  
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.lightBlue,
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.black, width: 2),
+            ),
+            child: Text(
+              'Hello Container!',
+              style: TextStyle(fontSize: 18, color: Colors.white),
             ),
           ),
         ),
-      ),
-    );
-  }
+              bottomNavigationBar: BottomNavigationBar(
+                currentIndex: index,
+                onTap: (i) => setState(() => index = i),
+                items: const [
+                  BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+                  BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+                  BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    ),
+);
+}
 }
